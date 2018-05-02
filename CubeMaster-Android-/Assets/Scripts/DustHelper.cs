36b947724d28a6126887ff6571f0f74cee5d010c
Darkmose <pohomov1997@gmail.com>
2018-05-02 12:02:36 +0300

    v.0.11

---
+++ CubeMaster-Android-/Assets/Scripts/DustHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DustHelper : MonoBehaviour
+{
+    GameObject particle;
+
+    private void Start()
+    {
+        particle = Resources.Load<GameObject>("Prefabs/Dust");
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Platform") || collision.CompareTag("Start") || collision.CompareTag("End"))
+        {
+            print("eee");
+            Instantiate(particle,collision.transform.position+transform.up*0.3f,Quaternion.identity);
+        }
+    }
+}
